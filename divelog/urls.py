28578e10ba06626f41a5858cbea0ef2bd091fe01
Ivan Habunek <ivan.habunek@gmail.com>
2012-07-20 12:13:32 +0200

    Simplified urls.py by using a prefix for patterns.

--- divelog/urls.py
+++ divelog/urls.py
@@ -4,33 +4,35 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^/?$', 'divelog.views.index'),
-    url(r'^gallery/$', 'divelog.views.gallery'),
+urlpatterns = patterns('divelog.views',
+    url(r'^/?$', 'index'),
+    url(r'^gallery/$', 'gallery'),
     
-    url(r'^settings/$', 'divelog.views.settings'),
-    url(r'^settings/account/$', 'divelog.views.settings_account'),
-    url(r'^settings/password/$', 'divelog.views.settings_password'),
+    url(r'^settings/$', 'settings'),
+    url(r'^settings/account/$', 'settings_account'),
+    url(r'^settings/password/$', 'settings_password'),
     
-    url(r'^dives/?$', 'divelog.views.dive_list'),
-    url(r'^dives/new/?$', 'divelog.views.dive_add'),
+    url(r'^dives/?$', 'dive_list'),
+    url(r'^dives/new/?$', 'dive_add'),
     
-    url(r'^dive/(?P<dive_id>\d+)/$', 'divelog.views.dive_view'),
-    url(r'^dive/(?P<dive_id>\d+)/edit/$', 'divelog.views.dive_edit'),
-    url(r'^dive/(?P<dive_id>\d+)/trash/$', 'divelog.views.dive_trash'),
-    url(r'^dive/(?P<dive_id>\d+)/restore/$', 'divelog.views.dive_restore'),
-    url(r'^dive/(?P<dive_id>\d+)/samples.json$', 'divelog.views.dive_samples_json'),
-    url(r'^dive/(?P<dive_id>\d+)/events.json$', 'divelog.views.dive_events_json'),
+    url(r'^dive/(?P<dive_id>\d+)/$', 'dive_view'),
+    url(r'^dive/(?P<dive_id>\d+)/edit/$', 'dive_edit'),
+    url(r'^dive/(?P<dive_id>\d+)/trash/$', 'dive_trash'),
+    url(r'^dive/(?P<dive_id>\d+)/restore/$', 'dive_restore'),
+    url(r'^dive/(?P<dive_id>\d+)/samples.json$', 'dive_samples_json'),
+    url(r'^dive/(?P<dive_id>\d+)/events.json$', 'dive_events_json'),
 
-    url(r'^locations/$', 'divelog.views.location_list'),
-    url(r'^location/(?P<location_id>\d+)/$', 'divelog.views.location_view'),
-    url(r'^location/(?P<location_id>\d+)/edit/$', 'divelog.views.location_edit'),
+    url(r'^locations/$', 'location_list'),
+    url(r'^location/(?P<location_id>\d+)/$', 'location_view'),
+    url(r'^location/(?P<location_id>\d+)/edit/$', 'location_edit'),
 
-    url(r'^uploads/$', 'divelog.views.upload_list'),
-    url(r'^upload/new/$', 'divelog.views.upload_add'),
-    url(r'^upload/import/$', 'divelog.views.upload_import'),
-    url(r'^upload/(?P<upload_id>\d+)/$', 'divelog.views.upload_view'),
-    
+    url(r'^uploads/$', 'upload_list'),
+    url(r'^upload/new/$', 'upload_add'),
+    url(r'^upload/import/$', 'upload_import'),
+    url(r'^upload/(?P<upload_id>\d+)/$', 'upload_view'),
+)
+
+urlpatterns += patterns('',
     url(r'^accounts/login/?$', 'django.contrib.auth.views.login', { 'template_name': 'accounts/login.html'}),
     url(r'^accounts/logout/?$', 'django.contrib.auth.views.logout', { 'next_page': '/'}),
     
