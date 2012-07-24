15079186459b18cf40a60ce2bf02c900e7e78755
Ivan Habunek <ivan.habunek@gmail.com>
2012-07-24 16:07:09 +0200

    Overriden default rendering of form errors.
    Achieved by extending ModelForm and creating a new ErrorList object which
    formats errors in the Bootstrap way.

--- divelog/forms.py
+++ divelog/forms.py
@@ -3,23 +3,48 @@ from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
 from django.forms.models import ModelForm
+from django.forms.util import ErrorList
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+from django.utils.encoding import force_unicode
 
-class DiveForm(ModelForm):
+class BootstrapErrorList(ErrorList):
+    def __unicode__(self):
+        return self.as_text()
+
+    def as_text(self):
+        if not self: return u''
+        errors = '<br />'.join(conditional_escape(force_unicode(e)) for e in self)
+        return mark_safe(u'<span class="help-inline">%s</span>' % errors)
+
+class BootstrapModelForm(ModelForm):
+    '''
+    Overrides the default model form and does the following:
+    - sets the error_class to BootstrapErrorList
+    '''
+    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None, initial=None,
+                 error_class=BootstrapErrorList, label_suffix=':', empty_permitted=False,
+                 instance=None):
+        super(BootstrapModelForm, self).__init__(data, files, auto_id, prefix, initial,
+                                                 error_class, label_suffix, empty_permitted,
+                                                 instance)
+
+class DiveForm(BootstrapModelForm):
     class Meta:
         model = Dive
         exclude = ('number', 'fingerprint', 'size', 'user')
 
-class DiveUploadForm(ModelForm):
+class DiveUploadForm(BootstrapModelForm):
     class Meta:
         model = DiveUpload
         exclude = ('uploaded', 'user')
 
-class LocationForm(ModelForm):
+class LocationForm(BootstrapModelForm):
     class Meta:
         model = Location
         exclude = ('user')
 
-class UserProfileForm(ModelForm):
+class UserProfileForm(BootstrapModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
